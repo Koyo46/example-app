70d9717401744bed7fe3d1e3297835fdde63efcf
reiken <ultee529@outlook.jp>
2023-04-05 17:26:32 +0900

    add Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\TestController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('tests/test',[TestController::class,'index']);
+
